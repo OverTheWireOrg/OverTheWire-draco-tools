4bb99044bf330c58b18c6fed3ee66935a80c0ccc
Steven Van Acker <steven@singularity.be>
2014-02-03 17:30:17 +0100

    fixing permissions

--- create-libvirt-vm.sh
+++ create-libvirt-vm.sh
@@ -14,4 +14,6 @@ ubuntu-vm-builder kvm precise --dest $target/$NAME --arch i386 --hostname $NAME 
 	--components main,universe,restricted --execscript="$bootstrap" \
 	--libvirt qemu:///system
 
+chown -R root.root $target/$NAME
+
 virsh start $NAME

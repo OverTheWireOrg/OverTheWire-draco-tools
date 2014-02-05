4b371771624144d8e1cc3550420f36052c865ac9
Steven Van Acker <steven@singularity.be>
2014-02-05 23:16:47 +0100

    build script update

--- build.sh
+++ build.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
 
-tmpdir=$(mktemp -d /tmp/XXXXXXXXXXXXXXXXXXXXX)
+conffile=$1
+vmtype=$2
 
+if [ ! -e "$conffile" ];
+then
+    echo "Usage: $0 <configfile> [libvirt|ova]"
+    echo "Given configfile not found"
+    exit 0
+fi
+
+if [ "$vmtype" != "libvirt" -a "$vmtype" != "ova"  ];
+then
+    echo "Usage: $0 <configfile> [libvirt|ova]"
+    echo "Given vmtype unknown"
+    exit 0
+fi
+
+if [ "$UID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+tmpdir=$(mktemp -d /tmp/XXXXXXXXXXXXXXXXXXXXX)
 rsync -rv --exclude=.git . $tmpdir
 
-find $tmpdir -type f -exec ./scripts/apply_template_inplace.py config/badidea.ini {} \;
+find $tmpdir -type f -exec ./scripts/apply_template_inplace.py "$conffile" {} \;
 
-#launch buildtools
-#maybe copy files somewhere
+here=$(pwd)
+cd $tmpdir
+case $vmtype in
+    libvirt)
+	./scripts/create-libvirt-vm.sh
+	;;
+    ova)
+	./scripts/make-ova-image.sh
+	;;
 
+cd $here
 
-#rm -rf $tmpdir
-echo look in $tmpdir
+rm -rf $tmpdir
+

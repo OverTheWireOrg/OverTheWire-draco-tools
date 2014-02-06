e0082aa28555a33049d0dc56981884158c34d671
Steven Van Acker <steven@singularity.be>
2014-02-06 16:07:05 +0100

    no more need for this since we're on bitbucket

--- copy-files-to-host.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script is only used to transfer files between a main host and a VM on Steven's laptop
-
-# copy all the files
-ussh -p 2243 0 "rm -rf /tmp/badidea && mkdir -p /tmp/badidea/"
-uscp -P 2243 -r . 0:/tmp/badidea/
-

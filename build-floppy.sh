ffd7f765719576e48cd1999b84770a19c317d247
Steven Van Acker <steven@singularity.be>
2014-03-19 11:03:51 +0100

    don't be directory dependent

--- build-floppy.sh
+++ build-floppy.sh
@@ -7,15 +7,16 @@ then
     exit 0
 fi
 
+distdir=$(dirname $(readlink -f $0))
 inf=$(readlink -f $1)
 key=$(readlink -f $2)
 outf=${3:-$inf.img}
 
 here=$(pwd)
 tmpdir=$(mktemp -d)
-mkdir -p network openvpn warzone
-cp -r floppyrootfs/* $tmpdir/
+cp -r $distdir/floppyrootfs/* $tmpdir/
 cd $tmpdir
+mkdir -p network openvpn warzone
 tar -xf $inf
 
 # extract username
@@ -26,14 +27,16 @@ mv interfaces.example network/interfaces
 
 # copy key and certificates to openvpn and warzone config
 cp $key openvpn/$username.key
+chmod go= openvpn/$username.key
 mv $username.crt $username.ca.crt $username.conf openvpn/
 mv $username.registry.crt $username.registry.ca.crt warzone/
 
 # make PKCS12 for the registry, empty password
 openssl pkcs12 -passout pass: -export -in warzone/$username.registry.crt -inkey openvpn/$username.key -out warzone/registry.p12
+chmod go= warzone/registry.p12
 
 cd $here
 
-sudo ./scripts/linux_dir_to_floppy_img.sh $tmpdir $outf
+sudo $distdir/scripts/linux_dir_to_floppy_img.sh $tmpdir $outf
 
 rm -rf $tmpdir
